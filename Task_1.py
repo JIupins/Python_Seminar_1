c27154ec4952fb3cd3e813d807233297393197d6
AlekseyBaboshin <BaboshinAP@gmail.com>
2023-01-30 00:23:21 +0300

    Created the file Task_2

---
+++ Task_1.py
@@ -0,0 +1,11 @@
+# Найдите сумму цифр трехзначного числа.
+from os import system
+system('cls')
+
+num_ini = int(input('Веедите целое число: '))
+num = num_ini
+sum = 0
+while num > 0:
+    sum += (num % 10)
+    num //= 10
+print(f'Сумма цифр в числе {num_ini} равна: {sum}')
